a7930a7eae380899ecef2b1779e64e7e1cc468e6
Thiago Dias <tdtadeu@gmail.com>
2013-12-11 22:08:31 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+J2c::Application.config.secret_key_base = '7a05112e1a31dbbabce7067627359775e950de84ed2b605025dd10bc2b1bcb4027e0320a729da3b82af50b619f6f46cf4e29b45e0821a783b70ca4d00a451398'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+J2c::Application.config.session_store :cookie_store, key: '_j2c_session'
