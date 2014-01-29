858e6f1360668da55fec706b6320569400034610
Thiago Dias <tdtadeu@gmail.com>
2014-01-29 08:32:11 -0200

    First strong params for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  before_action :set_agenda, only: [:index]
+  
   def index
   end
   
-  def set_agenda
+  def define_agenda
   end
+  
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_agenda
+      @agendas = params[:agendas]
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def application_params
+      params.require(:canvas).permit(:title, subjects_attributes: [:id, :title, :takeaway, :_destroy])
+    end
 end
