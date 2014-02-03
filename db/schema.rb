e18a5f98db1ed8ab4906b45ce9e89d4a8022272a
Thiago Dias <tdtadeu@gmail.com>
2014-02-03 20:07:00 -0200

    Reorganizing stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140203210706) do
+ActiveRecord::Schema.define(version: 20140203215858) do
 
   create_table "canvases", force: true do |t|
     t.string   "title"
