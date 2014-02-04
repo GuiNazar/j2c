04834b9cdfc2ef6150d05b223d3734ef8a73c5cc
Thiago Dias <tdtadeu@gmail.com>
2014-02-04 05:40:44 -0200

    Refactored a little code in move_up; Blocks now open edit page on modal window

--- app/models/block.rb
+++ app/models/block.rb
@@ -8,13 +8,15 @@ class Block < ActiveRecord::Base
   end
   
   def move_up!
-    current_position = self.position
-    replacing = Block.order(position: :desc).where("position < :pstn", {pstn: current_position}).take
-    unless replacing.nil?
-      self.position = replacing.position
-      replacing.position = current_position
+    previous = previous_block
+    unless previous.nil?
+      self.position = previous.position
+      previous.update(position: self.position_was)
       self.save
-      replacing.save
     end
+  end
+  
+  def previous_block
+    previous = Block.order(position: :desc).where("position < :pstn", {pstn: self.position}).take
   end
 end
